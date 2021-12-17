a2c1555ccffa4e675fb57440b5e54401cd9edb2e
Fayez-fyz <zeyaffayez007@gmail.com>
2021-12-17 11:48:38 +0530

    intial-commit

---
+++ components/CribsData.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function CribsData(props) {
+  return (
+      <div class="col">
+      <div className="card ">
+        <img
+          className="card-img-top"
+          width="200"
+          height="200"
+          src={props.pic}
+          alt="pic"
+        />
+        <div className="card-body">
+          <h5 className="card-title  d-flex justify-content-center">
+            {props.name}
+          </h5>
+          <h5 className="card-text d-flex justify-content-center">
+            {props.location}
+          </h5>
+          <div className="text-center">
+            <button
+              className="btn btn-outline-primary mx-2 "
+              onClick={() =>{
+                props.setok(true)
+                props.edit(props.id, props.name, props.pic, props.location)}
+                
+              }
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-outline-danger mx-2 "
+              onClick={() => props.delete(props.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
+
+  );
+}
+
+export default CribsData;
---
+++ pages/cribs.js
@@ -0,0 +1,199 @@
+import React, { useContext } from "react";
+import { useState, useEffect } from "react";
+import { Modal } from "antd";
+import axios from "axios";
+import CribsData from "../components/CribsData";
+import UserRoute from "../components/routes/UserRoute";
+import { UserContext } from "../context";
+import { PlusSquareOutlined } from "@ant-design/icons";
+
+const Cribs = () => {
+  const [state, setState] = useContext(UserContext);
+  const [cribs, setCribs] = useState([]);
+  const [name, setName] = useState("");
+  const [img, setImg] = useState("");
+  const [id, setid] = useState(null);
+  const [ok, setok] = useState(false);
+  const [pass, setPass] = useState(true);
+  const [location, setLocation] = useState("");
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  let GetData = async () => {
+    try {
+      const cribs = await axios.get(
+        "/cribs"
+      );
+      setCribs([...cribs.data]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let handleCreate = async () => {
+    try {
+      const create = await axios.post(
+        "/cribs",
+        {
+          name: name,
+          pic: img,
+          location: location,
+        }
+      );
+      setName("");
+      setImg("");
+      setLocation("");
+      GetData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  let handleUpdate = async (id) => {
+    try {
+      const update = await axios.put(
+        `/cribs/${id}`,
+        {
+          name: name,
+          pic: img,
+          location: location,
+        }
+      );
+      setName("");
+      setImg("");
+      setLocation("");
+      GetData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  let handleEdit = (id, name, pic, location) => {
+    setPass(!pass);
+    setid(id);
+    pass ? setName(name) : setName("");
+    pass ? setImg(pic) : setImg("");
+    pass ? setLocation(location) : setLocation("");
+  };
+
+  let handleDelete = async (id) => {
+    try {
+      const del = await axios.delete(
+        `/cribs/${id}`
+      );
+      GetData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (state && state.token) {
+      GetData();
+    }
+  }, [state && state.token]);
+
+  return (
+  <UserRoute>
+   <div className="container-fluid my-3">
+   <div className="row">
+   
+     <div className="col-lg-12">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="form-control"
+            placeholder="Search by name ..."
+            aria-label="Recipient's name"
+            aria-describedby="basic-addon2"
+          />
+          <div className="input-group-append">
+            <button className="btn btn-outline-secondary" type="button">
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center my-2">
+              <button className="btn btn-secondary" name='Create Cribs'  onClick={() => setok(true)}>
+              <PlusSquareOutlined className=" fs-2 " /> <span>Create Cribs</span>
+              </button>
+            </div>
+        <Modal
+         title="Create/Edit"
+         visible={ok}
+         onCancel={() => setok(false)}
+         footer={null}>
+        <div >
+        <h3> Create / Edit </h3>
+        <input
+          value={name}
+          className="form-control mt-2"
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+
+        <input
+          value={img}
+          className="form-control mt-2"
+          onChange={(e) => setImg(e.target.value)}
+          type="text"
+          placeholder="Image Url"
+        />
+
+        <input
+          value={location}
+          className="form-control mt-2"
+          onChange={(e) => setLocation(e.target.value)}
+          type="text"
+          placeholder="Location"
+        />
+
+        <button
+          className="btn btn-block btn-primary mt-2 mb-2"
+          onClick={() =>
+            name !== false && img !== false && location !== false
+              ? pass
+                ? handleCreate()
+                : handleUpdate(id)
+              : ""
+          }
+        >
+          Submit
+        </button>
+      </div>
+        </Modal>
+        {loading ? (
+          <img
+            src="https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e47a3p0udgs59ya5nzo9fy2sirdmd88v3sgc322j1mm&rid=giphy.gif&ct=g"
+            alt="loading"
+            className='mx-auto mt-3 d-block'
+          ></img>
+        ) : (
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {cribs
+              .filter((obj) => obj.name.includes(search))
+              .map((obj, id) => (
+                <CribsData
+                  key={id}
+                  id={obj._id}
+                  name={obj.name}
+                  pic={obj.pic}
+                  location={obj.location}
+                  edit={handleEdit}
+                  update={handleUpdate}
+                  delete={handleDelete}
+                  ok={ok}
+                  setok={setok}
+                />
+              ))}
+          </div>
+        )}
+      </div>
+    </div>
+   </div>
+  </UserRoute>
+  );
+};
+
+export default Cribs;
